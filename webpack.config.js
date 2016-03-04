e4a72ef268a14dbd5ec2a1a6cbde8e96122899b2
ebinhon <bin.hong@ericsson.com>
2016-03-04 17:50:25 +0800

    add asset file in build directory

--- webpack.config.js
+++ webpack.config.js
@@ -5,13 +5,18 @@ var pathToReact = path.resolve(node_modules, 'react/dist/react.min.js');
 
 //独立打包样式文件
 var ExtractTextPlugin = require("extract-text-webpack-plugin");
+//拷贝文件
+var TransferWebpackPlugin = require('transfer-webpack-plugin');
 
 var debug = process.env.NODE_ENV !== "prod"; // or dev
 
 var config = {
     plugins: [
         /*new webpack.HotModuleReplacementPlugin(),*/
-        new ExtractTextPlugin("style.css")
+        new ExtractTextPlugin("style.css"),
+        new TransferWebpackPlugin([
+            {from: 'Asset',to:'Asset'}
+        ], path.resolve(__dirname,"src"))
     ],
 
     entry: debug ? ['webpack/hot/dev-server',path.resolve(__dirname, 'src/App.js')] : [path.resolve(__dirname, 'src/App.js')],
